fdafd8d3581937fdb3969d802652722186595a68
Joshua Shinavier <josh@fortytwo.net>
2014-04-03 07:11:19 -0400

    Added Extendo WebDAV project

---
+++ extendo-webdav/src/main/java/net/fortytwo/extendo/webdav/SecondHop.java
@@ -0,0 +1,51 @@
+package net.fortytwo.extendo.webdav;
+
+import org.apache.jackrabbit.core.TransientRepository;
+
+import javax.jcr.Node;
+import javax.jcr.Repository;
+import javax.jcr.Session;
+import javax.jcr.SimpleCredentials;
+
+/**
+ * Second hop example. Stores, retrieves, and removes example content.
+ */
+public class SecondHop {
+
+    /**
+     * The main entry point of the example application.
+     *
+     * @param args command line arguments (ignored)
+     * @throws Exception if an error occurs
+     */
+    public static void main(String[] args) throws Exception {
+        System.setProperty("org.apache.jackrabbit.repository.home", "/tmp/repo");
+
+        Repository repository = new TransientRepository();
+        Session session = repository.login(
+                new SimpleCredentials("username", "password".toCharArray()));
+        try {
+            Node root = session.getRootNode();
+
+            // Store content
+            /*
+            Node hello = root.addNode("hello");
+            Node world = hello.addNode("world");
+            world.setProperty("message", "Hello, World!");
+            session.save();
+            */
+
+            // Retrieve content
+            Node node = root.getNode("hello/world");
+            System.out.println(node.getPath());
+            System.out.println(node.getProperty("message").getString());
+
+            // Remove content
+            //root.getNode("hello").remove();
+            session.save();
+        } finally {
+            session.logout();
+        }
+    }
+
+}
---
+++ extendo-webdav/src/main/java/net/fortytwo/extendo/webdav/WebdavPlay.java
@@ -0,0 +1,13 @@
+package net.fortytwo.extendo.webdav;
+
+import org.apache.jackrabbit.webdav.simple.SimpleWebdavServlet;
+
+/**
+ * @author Joshua Shinavier (http://fortytwo.net)
+ */
+public class WebdavPlay {
+    private static void play() throws Exception {
+        //JCRWebdavServerServlet servlet;
+        SimpleWebdavServlet servlet;
+    }
+}
